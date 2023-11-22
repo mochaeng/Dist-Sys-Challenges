77955d5862229f7672f3bbca4b503072f5838574
Mochaeng <campos.antoonio@gmail.com>
2023-11-22 13:00:34 -0300

    3d challenge

--- 03d-efficient-broadcast/README.md
+++ 03d-efficient-broadcast/README.md
@@ -1,1 +1,15 @@
-https://github.com/jepsen-io/maelstrom/blob/main/doc/03-broadcast/02-performance.md
+[link-doc](https://github.com/jepsen-io/maelstrom/blob/main/doc/03-broadcast/02-performance.md)
+
+# Running
+
+I only got able to solve it by using a custom topology: `--topology tree4`
+
+Results are on average:
+
+- Median latency: 388~410ms
+- Maximum latency: 590~600ms
+- Messages per-op: 29
+
+```sh
+maelstrom test -w broadcast --bin ~/go/bin/maelstrom-broadcast --node-count 25 --time-limit 20 --rate 100 --latency 100 --topology tree4
+```
--- 03d-efficient-broadcast/main.go
+++ 03d-efficient-broadcast/main.go
@@ -10,8 +10,20 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type BroadcastRequest struct {
+	TP     string `json:"type"`
+	Value  int    `json:"message"`
+	Values []int  `json:"messages"`
+	ID     *int   `json:"msg_id"`
+}
+
+type TopologyRequest struct {
+	Topology map[string][]string `json:"topology"`
+}
+
 type Worker struct {
-	values       mapset.Set[int]
+	Node         *maelstrom.Node
+	Values       mapset.Set[int]
 	ValuesKnowBy map[string]mapset.Set[int]
 
 	SeenValues map[int]bool
@@ -23,12 +35,12 @@ type Worker struct {
 }
 
 func (w *Worker) AddValue(value int) {
-	w.values.Add(value)
+	w.Values.Add(value)
 }
 
 func (w *Worker) AddValues(values []int) {
 	for _, value := range values {
-		w.values.Add(value)
+		w.Values.Add(value)
 	}
 }
 
@@ -39,10 +51,11 @@ func (w *Worker) AddValuesKnowByNeighbor(neighborID string, values []int) {
 }
 
 func (w *Worker) HandleValueSendByClient(neighboorID string, value int) {
-	w.values.Add(value)
+	w.Values.Add(value)
 
 	w.ValMutex.Lock()
 	defer w.ValMutex.Unlock()
+
 	_, ok := w.ValuesKnowBy[neighboorID]
 	if !ok {
 		w.ValuesKnowBy[neighboorID] = mapset.NewSet[int]()
@@ -55,6 +68,7 @@ func (w *Worker) HandleValuesSendByWorker(neighboorID string, values []int) {
 
 	w.ValMutex.Lock()
 	defer w.ValMutex.Unlock()
+
 	_, ok := w.ValuesKnowBy[neighboorID]
 	if !ok {
 		w.ValuesKnowBy[neighboorID] = mapset.NewSet[int]()
@@ -62,156 +76,111 @@ func (w *Worker) HandleValuesSendByWorker(neighboorID string, values []int) {
 	w.AddValuesKnowByNeighbor(neighboorID, values)
 }
 
-type BroadcastRequest struct {
-	TP     string `json:"type"`
-	Value  int    `json:"message"`
-	Values []int  `json:"messages"`
-	ID     *int   `json:"msg_id"`
+func (w *Worker) onHandleInit(msg maelstrom.Message) error {
+	go func() {
+		ticker := time.Tick(time.Duration(25 * time.Millisecond))
+		for range ticker {
+
+			for _, neighborID := range w.Neighbors {
+				neighbor := neighborID
+
+				w.ValMutex.Lock()
+				valuesKnowBy := w.ValuesKnowBy[neighbor]
+				w.ValMutex.Unlock()
+
+				valuesToSend := w.Values.Difference(valuesKnowBy).ToSlice()
+
+				if len(valuesToSend) == 0 {
+					continue
+				}
+
+				msgToGoosip := BroadcastRequest{
+					TP:     "broadcast",
+					Values: valuesToSend,
+				}
+
+				w.Node.RPC(neighbor, msgToGoosip, func(msg maelstrom.Message) error {
+					w.ValMutex.Lock()
+					w.AddValuesKnowByNeighbor(msg.Src, valuesToSend)
+					w.ValMutex.Unlock()
+					return nil
+				})
+			}
+
+		}
+	}()
+
+	return nil
 }
 
-type TopologyRequest struct {
-	Topology map[string][]string `json:"topology"`
-}
+func (w *Worker) onHandleBroadcast(msg maelstrom.Message) error {
+	broadcastMSG := BroadcastRequest{}
+	json.Unmarshal(msg.Body, &broadcastMSG)
 
-func (w *Worker) SendRPCWithRetries(numRetries int) {
-	for i := 0; i < numRetries; i++ {
+	if broadcastMSG.Values != nil {
+		w.HandleValuesSendByWorker(msg.Src, broadcastMSG.Values)
+	} else {
+		w.HandleValueSendByClient(msg.Src, broadcastMSG.Value)
 	}
+
+	response := map[string]any{
+		"type": "broadcast_ok",
+	}
+
+	return w.Node.Reply(msg, response)
+}
+
+func (w *Worker) onHandleTopology(msg maelstrom.Message) error {
+	topologyMSG := TopologyRequest{}
+	if err := json.Unmarshal(msg.Body, &topologyMSG); err != nil {
+		return err
+	}
+
+	w.TopoMutex.Lock()
+	w.Neighbors = topologyMSG.Topology[w.Node.ID()]
+	w.TopoMutex.Unlock()
+
+	w.ValMutex.Lock()
+	for _, neighborID := range w.Neighbors {
+		w.ValuesKnowBy[neighborID] = mapset.NewSet[int]()
+	}
+	w.ValMutex.Unlock()
+
+	response := map[string]any{
+		"type": "topology_ok",
+	}
+
+	return w.Node.Reply(msg, response)
+}
+
+func (w *Worker) onHandleRead(msg maelstrom.Message) error {
+	values := w.Values.ToSlice()
+
+	response := map[string]any{
+		"type":     "read_ok",
+		"messages": values,
+	}
+
+	return w.Node.Reply(msg, response)
 }
 
 func main() {
 	node := maelstrom.NewNode()
 
 	worker := Worker{
-		values:       mapset.NewSet[int](),
+		Node:         node,
+		Values:       mapset.NewSet[int](),
 		ValuesKnowBy: make(map[string]mapset.Set[int]),
 		SeenValues:   make(map[int]bool),
 		Neighbors:    []string{},
 	}
 
-	node.Handle("init", func(msg maelstrom.Message) error {
-		go func() {
+	worker.Node.Handle("init", worker.onHandleInit)
+	worker.Node.Handle("broadcast", worker.onHandleBroadcast)
+	worker.Node.Handle("topology", worker.onHandleTopology)
+	worker.Node.Handle("read", worker.onHandleRead)
 
-			ticker := time.Tick(time.Duration(115) * time.Millisecond)
-			for range ticker {
-
-				for _, neighborID := range worker.Neighbors {
-					// worker.ValMutex.Lock()
-					// if _, ok := worker.ValuesKnowBy[neighborID]; !ok {
-					// 	continue
-					// }
-					diffSet := worker.values.Difference(worker.ValuesKnowBy[neighborID])
-					// worker.ValMutex.Unlock()
-					valuesToSend := diffSet.ToSlice()
-
-					if len(valuesToSend) == 0 {
-						continue
-					}
-
-					msgToGoosip := BroadcastRequest{
-						TP:     "broadcast",
-						Values: valuesToSend,
-					}
-					node.Send(neighborID, msgToGoosip)
-
-					// node.RPC(neighborID, msgToGoosip, func(msg maelstrom.Message) error {
-					// 	worker.ValMutex.Lock()
-					// 	fmt.Fprintf(os.Stderr, "Meus vizinhos: [%v] o cara me respondeu [%v] deveria ser [%v]: \n", worker.Neighbors, neighborID, neighborID)
-					// 	worker.AddValuesKnowByNeighbor(neighborID, valuesToSend)
-					// 	worker.ValMutex.Unlock()
-					// 	return nil
-					// })
-					// tentar utilizar RPC
-					// quando chegar a confirmação de envio, salva no set
-				}
-
-				// for _, value := range values.ToSlice() {
-
-				// 	worker.TopoMutex.Lock()
-				// 	neighbors := worker.Neighbors
-				// 	worker.TopoMutex.Unlock()
-
-				// 	for _, neighboorID := range neighbors {
-				// 		worker.ValMutex.Lock()
-				// isValueKnowByNeighbor := worker.ValueKnowBy[neighboorID][value]
-				// worker.ValMutex.Unlock()
-
-				// if !isValueKnowByNeighbor {
-				// 	// node.Send(neighboorID, msgToGoosip)
-				// 	node.RPC(neighboorID, msgToGoosip, func(msg maelstrom.Message) error {
-				// 		worker.ValMutex.Lock()
-				// 		_, ok := worker.ValueKnowBy[neighboorID]
-				// 		if !ok {
-				// 			worker.ValueKnowBy[neighboorID] = make(map[int]bool)
-				// 		}
-				// 		worker.ValueKnowBy[neighboorID][value] = true
-				// 		worker.ValMutex.Unlock()
-				// 		return nil
-				// 	})
-				// }
-			}
-			// }
-			// }
-		}()
-		return nil
-	})
-
-	node.Handle("broadcast", func(msg maelstrom.Message) error {
-		broadcastMSG := BroadcastRequest{}
-		json.Unmarshal(msg.Body, &broadcastMSG)
-
-		if broadcastMSG.Values != nil {
-			worker.HandleValuesSendByWorker(msg.Src, broadcastMSG.Values)
-		} else {
-			worker.HandleValueSendByClient(msg.Src, broadcastMSG.Value)
-		}
-
-		response := map[string]any{
-			"type": "broadcast_ok",
-		}
-
-		if broadcastMSG.ID != nil {
-			return node.Reply(msg, response)
-		}
-		return nil
-	})
-
-	node.Handle("read", func(msg maelstrom.Message) error {
-		// worker.ValMutex.Lock()
-		values := worker.values
-		// worker.ValMutex.Unlock()
-
-		response := map[string]any{
-			"type":     "read_ok",
-			"messages": values,
-		}
-
-		return node.Reply(msg, response)
-	})
-
-	node.Handle("topology", func(msg maelstrom.Message) error {
-		topologyMSG := TopologyRequest{}
-		if err := json.Unmarshal(msg.Body, &topologyMSG); err != nil {
-			return err
-		}
-
-		worker.TopoMutex.Lock()
-		worker.Neighbors = topologyMSG.Topology[node.ID()]
-		worker.TopoMutex.Unlock()
-
-		worker.ValMutex.Lock()
-		for _, neighborID := range worker.Neighbors {
-			worker.ValuesKnowBy[neighborID] = mapset.NewSet[int]()
-		}
-		worker.ValMutex.Unlock()
-
-		response := map[string]any{
-			"type": "topology_ok",
-		}
-
-		return node.Reply(msg, response)
-	})
-
-	if err := node.Run(); err != nil {
+	if err := worker.Node.Run(); err != nil {
 		log.Fatal("Error: ", err)
 	}
 
